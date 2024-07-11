6550411c40c3f0b2ec6e794c5057562ab80ccb9d
minarai-niwashi <keyaki.ann@gmail.com>
2024-07-11 02:24:52 +0000

    postCreateCommand.shファイルを更新

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y python3-pip curl git sudo
-
 python3 -m pip --no-cache-dir install -r requirements-custom.txt
 python3 -m pip --no-cache-dir install -r requirements.txt
