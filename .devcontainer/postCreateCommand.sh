1bf7e9ef7b1eb507b47ed3268dce9dd6d0ff142b
minarai-niwashi <keyaki.ann@gmail.com>
2024-07-12 00:00:19 +0000

    postCreateCommand.shファイルを更新

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export PATH=$HOME/.local/bin:$PATH
 python3 -m pip --no-cache-dir install -r requirements-custom.txt
 python3 -m pip --no-cache-dir install -r requirements.txt
+pre-commit install
