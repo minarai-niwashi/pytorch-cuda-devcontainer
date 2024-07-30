480c4fb8441baabf90d489a566c9483cbdf873f3
minarai-niwashi <58800220+minarai-niwashi@users.noreply.github.com>
2024-07-30 19:18:41 +0900

    pyproject.tomlの導入 (#17)
    close #13

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export PATH=$HOME/.local/bin:$PATH
-python3 -m pip --no-cache-dir install -r requirements-custom.txt
-python3 -m pip --no-cache-dir install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip --no-cache-dir install .[dev]
 pre-commit install
